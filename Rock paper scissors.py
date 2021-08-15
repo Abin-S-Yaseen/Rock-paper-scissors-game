b0b775c689e61f03d65ed0dec25dd5721f5b52be
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-08-16 01:52:48 +0530

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -4,3 +4,11 @@ import tkinter
 from tkinter import *
 import random
 from PIL import ImageTk,Image
+
+# Setting up the window
+root = Tk()
+root.geometry('900x650')
+root.maxsize(900,650)
+root.minsize(900,580)
+root.configure(background='#9999FF')
+root.title('Rock paper scissors')
