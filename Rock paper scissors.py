ab840990523676c19f9f95ae08b6f4785b25669f
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-09-05 00:09:51 +0530

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -89,3 +89,11 @@ def play_game(user_selection):
             sys_paper_img.config(bg='white')
             sys_scissors_img.config(bg='#9999FF')
 
+        if choice == 'scissors':
+            sys_rock_img.config(bg='#9999FF')
+            sys_paper_img.config(bg='#9999FF')
+            sys_scissors_img.config(bg='white')
+
+        user_rock_img.config(bg='#9999FF')
+        user_paper_img.config(bg='#9999FF')
+        user_scissors_img.config(bg='white')
